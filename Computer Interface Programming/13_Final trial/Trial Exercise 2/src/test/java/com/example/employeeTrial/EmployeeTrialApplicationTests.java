8684ea3a6325a4ad8d05498827db5d0b1059c0e7
QuiteNew <pandilovskikonstantin@gmail.com>
2026-06-12 18:56:07 +0200

    .

---
+++ Computer Interface Programming/13_Final trial/Trial Exercise 2/src/test/java/com/example/employeeTrial/EmployeeTrialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeeTrial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeTrialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
